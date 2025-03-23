1710b5c023159200b4fd5fb9a4ff8c48ac4c8ddc
abdulhamidnugroho <abdulhamidnugroho@gmail.com>
2025-03-23 16:05:42 +0700

    update: main fields (ChatCompletionRequest) documentation

--- openai/openai.go
+++ openai/openai.go
@@ -14,37 +14,131 @@ import (
 // TODO(seungduk): Auto-generate this file from the OpenAI API reference.
 
 type ChatCompletionRequest struct {
-	Messages            []Message             `json:"messages"`
-	Model               string                `json:"model"`
-	FrequencyPenalty    *float32              `json:"frequency_penalty,omitempty"`
-	LogitBias           map[string]float32    `json:"logit_bias,omitempty"`
-	Logprobs            *bool                 `json:"logprobs,omitempty"`
-	TopLogprobs         *int32                `json:"top_logprobs,omitempty"`
-	MaxTokens           *int32                `json:"max_tokens,omitempty"`
-	MaxCompletionTokens *int32                `json:"max_completion_tokens,omitempty"`
-	CandidateCount      *int32                `json:"n,omitempty"`
-	PresencePenalty     *float32              `json:"presence_penalty,omitempty"`
-	ResponseFormat      *ResponseFormat       `json:"response_format,omitempty"`
-	Seed                *int32                `json:"seed,omitempty"`
-	ServiceTier         *string               `json:"service_tier,omitempty"`
-	StopSequences       *StopSequences        `json:"stop,omitempty"`
-	Stream              *bool                 `json:"stream,omitempty"`
-	StreamOptions       *StreamOptions        `json:"stream_options,omitempty"`
-	Temperature         *float32              `json:"temperature,omitempty"`
-	TopP                *float32              `json:"top_p,omitempty"`
-	Tools               []Tool                `json:"tools,omitempty"`
-	ToolChoice          *ToolChoice           `json:"tool_choice,omitempty"`
-	ParallelToolCalls   *bool                 `json:"parallel_tool_calls,omitempty"`
-	User                *string               `json:"user,omitempty"`
-	FunctionCall        *LegacyFunctionChoice `json:"function_call,omitempty"`
-	Functions           []LegacyFunction      `json:"functions,omitempty"`
-	Audio               *Audio                `json:"audio,omitempty"`
-	Metadata            *orderedmap.Map       `json:"metadata,omitempty"` // Maximum: 16 chars for key, 512 chars for value
-	Modalities          *[]string             `json:"modalities,omitempty"`
-	Prediction          *Prediction           `json:"prediction,omitempty"`
-	ReasoningEffort     *string               `json:"reasoning_effort,omitempty"`
-	Store               *bool                 `json:"store,omitempty"`
-	WebSearchOptions    *WebSearchOptions     `json:"web_search_options,omitempty"`
+	// A list of messages comprising the conversation so far. Depending on the
+	// [model](https://platform.openai.com/docs/models) you use, different message
+	// types (modalities) are supported, like [text], [images], and [audio].
+	Messages []Message `json:"messages"`
+
+	Model string `json:"model"`
+
+	// Should be between -2.0 and 2.0
+	FrequencyPenalty *float32 `json:"frequency_penalty,omitempty"`
+
+	// A JSON object that maps tokens (specified by their token ID in the
+	// tokenizer) to an associated bias value from -100 to 100.
+	// Values between -1 and 1 should decrease or increase likelihood of selection;
+	// Values like -100 or 100 should result in a ban or exclusive selection of the relevant token.
+	LogitBias map[string]float32 `json:"logit_bias,omitempty"`
+
+	// If true, returns the log probabilities of each output token returned in the `content` of `message`.
+	Logprobs *bool `json:"logprobs,omitempty"`
+
+	// An integer between 0 and 20 specifying the number of most likely tokens to
+	// return at each token position, `logprobs` must be set to `true` if this parameter is used.
+	TopLogprobs *int32 `json:"top_logprobs,omitempty"`
+
+	// Deprecated: This value is now deprecated in favor of max_completion_tokens,
+	// Not compatible with o1 series models
+	MaxTokens *int32 `json:"max_tokens,omitempty"`
+
+	// An upper bound for the number of tokens that can be generated for a completion,
+	// including visible output tokens and [reasoning tokens](https://platform.openai.com/docs/guides/reasoning).
+	MaxCompletionTokens *int32 `json:"max_completion_tokens,omitempty"`
+
+	// How many chat completion choices to generate for each input message.
+	CandidateCount *int32 `json:"n,omitempty"`
+
+	// Number between -2.0 and 2.0. Positive values penalize new tokens based on
+	// whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+	PresencePenalty *float32 `json:"presence_penalty,omitempty"`
+
+	// An object specifying the format that the model must output.
+	// Setting to `{ "type": "json_schema", "json_schema": {...} }` enables Structured
+	// Outputs which ensures the model will match your supplied JSON schema.
+	// Setting to `{ "type": "json_object" }` enables the older JSON mode, which ensures the message
+	// is valid JSON. Using `json_schema` is preferred for models that support it.
+	ResponseFormat *ResponseFormat `json:"response_format,omitempty"`
+
+	Seed *int32 `json:"seed,omitempty"`
+
+	// Specifies the latency tier to use for processing the request. This parameter is
+	// relevant for customers subscribed to the scale tier service:
+	// - If set to 'auto', and the Project is Scale tier enabled, the system will utilize scale tier credits until they are exhausted.
+	// - If set to 'auto', and the Project is not Scale tier enabled, the request will be processed using the default service tier
+	//   with a lower uptime SLA and no latency guarentee.
+	// - If set to 'default', the request will be processed using the default service tier
+	//   with a lower uptime SLA and no latency guarentee.
+	// - When not set, the default behavior is 'auto'.
+	// When this parameter is set, the response body will include the `service_tier` utilized.
+	ServiceTier *string `json:"service_tier,omitempty"`
+
+	// Up to 4 sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence.
+	StopSequences *StopSequences `json:"stop,omitempty"`
+
+	// If set to true, the model response data will be streamed to the client as it is generated using server-sent events.
+	Stream *bool `json:"stream,omitempty"`
+
+	// Options for streaming response. Only set this when you set `stream: true`.
+	StreamOptions *StreamOptions `json:"stream_options,omitempty"`
+
+	// Between 0 and 2. Higher values like 0.8 will make the output more random,
+	// while lower values like 0.2 will make it more focused and deterministic.
+	// Generally recommend altering this or `top_p` but not both.
+	Temperature *float32 `json:"temperature,omitempty"`
+
+	// An alternative to sampling with temperature, the model considers the results of the tokens with top_p probability mass.
+	// 0.1 means only the tokens comprising the top 10% probability mass are considered.
+	// Generally recommend altering this or `temperature` but not both.
+	TopP *float32 `json:"top_p,omitempty"`
+
+	// Currently, only functions are supported as a tool.
+	// This provides a list of functions the model may generate JSON inputs for.
+	// Max of 128 functions are supported.
+	Tools []Tool `json:"tools,omitempty"`
+
+	// Controls which tool is called by the model. `none` means the model will
+	// not call any tool and instead generates a message. `auto` means the model can
+	// pick between generating a message or calling one or more tools. `required` means
+	// the model must call one or more tools.
+	// `auto` is the default if tools are present.
+	ToolChoice *ToolChoice `json:"tool_choice,omitempty"`
+
+	// Whether to enable parallel function calling during tool use.
+	ParallelToolCalls *bool `json:"parallel_tool_calls,omitempty"`
+
+	// A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse.
+	User *string `json:"user,omitempty"`
+
+	// Deprecated in favor of `tool_choice`.
+	FunctionCall *LegacyFunctionChoice `json:"function_call,omitempty"`
+
+	// Deprecated in favor of `tools`.
+	Functions []LegacyFunction `json:"functions,omitempty"`
+
+	// Required when audio output is requested with `modalities: ["audio"]`.
+	Audio *Audio `json:"audio,omitempty"`
+
+	// Maximum: 16 chars for key, 512 chars for value
+	Metadata *orderedmap.Map `json:"metadata,omitempty"`
+
+	// Output types that you would like the model to generate. Default: `["text"]`
+	// To request that this model generate both text and audio responses, we can use: `["text", "audio"]`
+	Modalities *[]string `json:"modalities,omitempty"`
+
+	Prediction *Prediction `json:"prediction,omitempty"`
+
+	// **o-series models only**
+	// Constrains effort on reasoning for [reasoning models](https://platform.openai.com/docs/guides/reasoning).
+	// Currently supported values are `low`, `medium`, and `high`.
+	// Reducing reasoning effort can result in faster responses and fewer tokens used on reasoning in a response.
+	ReasoningEffort *string `json:"reasoning_effort,omitempty"`
+
+	// Whether or not to store the output of this chat completion request for use in
+	// [model distillation](https://platform.openai.com/docs/guides/distillation)
+	// or [evals](https://platform.openai.com/docs/guides/evals) products.
+	Store *bool `json:"store,omitempty"`
+
+	WebSearchOptions *WebSearchOptions `json:"web_search_options,omitempty"`
 }
 
 type Prediction struct {
